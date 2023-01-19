d5c63ef819a8830db353409d6b677c0a3cc7742b
jeremydeblaecker <jeremy.deblaecker@gmail.com>
2023-01-19 15:29:44 +0100

    ajout images

--- app/src/main/java/com/chillcoding/MainActivity.kt
+++ app/src/main/java/com/chillcoding/MainActivity.kt
@@ -2,10 +2,43 @@ package com.chillcoding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.content.res.Resources.getSystem
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_oncreate))
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_onrestart))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_onstart))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_onresume))
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_onpause))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_onstop))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(MainActivity::class.simpleName, getString(R.string.text_ondestroy))
     }
 }
