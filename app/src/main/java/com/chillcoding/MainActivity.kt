3b5fbb45ae4a7824cce7d586d5a3c726c8c5b31c
jeremydeblaecker <jeremy.deblaecker@gmail.com>
2023-02-08 13:57:29 +0100

    Ajout de du recycler view beach

--- app/src/main/java/com/chillcoding/MainActivity.kt
+++ app/src/main/java/com/chillcoding/MainActivity.kt
@@ -7,9 +7,14 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import android.content.res.Resources.getSystem
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     // Define the layout
@@ -18,7 +23,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         onClicked()
         Log.i(MainActivity::class.simpleName, getString(R.string.text_oncreate))
+        // Show the recycler view link with the adapter
+        val recyclerView = findViewById<RecyclerView>(R.id.beach_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = BeachAdapter(resources.getStringArray(R.array.name))
     }
+
     // When mainButton is clicked show toast hi
     fun onClicked(){
         var button = findViewById<Button>(R.id.mainButton)
@@ -65,4 +75,21 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.i(MainActivity::class.simpleName, getString(R.string.text_ondestroy))
     }
+}
+class BeachAdapter(private val beachList: Array<String>) :
+    RecyclerView.Adapter<BeachAdapter.BeachViewHolder>() {
+
+    class BeachViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    // Return the list of beach
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeachViewHolder {
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(android.R.layout.simple_list_item_1, parent, false) as TextView
+        return BeachViewHolder(textView)
+    }
+
+    override fun onBindViewHolder(holder: BeachViewHolder, position: Int) {
+        holder.textView.text = beachList[position]
+    }
+
+    override fun getItemCount() = beachList.size
 }
